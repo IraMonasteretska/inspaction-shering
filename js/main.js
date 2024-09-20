cb7155842c2ab46bace36066c18769c94e288c28
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2024-09-20 15:38:23 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,48 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('.searchbtn').click(function(){
+    $('.searchbtn').click(function () {
         $(this).next('form').toggle();
+    });
+
+    // mob menu
+    $('.menubtn').click(function () {
+        $('.menubg').addClass('show');
+        $('.mobmenu').addClass('open');
+    });
+    $('.closemenu').click(function () {
+        $('.menubg').removeClass('show');
+        $('.mobmenu').removeClass('open');
+    });
+
+
+    // swiper
+    var swiper = new Swiper(".mySwiper", {
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            dynamicBullets: true,
+        },
     });
 
 
 
+
+
+
+
+
+
+
+
+
+    // ------------------------------
+
+
+
+
 });
+
