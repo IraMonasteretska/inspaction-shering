1c03b7eca6cb96cf642b595957401424be9421ff
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2024-09-24 17:58:37 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ $(document).ready(function () {
     if ($('.prodslider').length) {
         var swiper = new Swiper(".prodslider", {
             slidesPerView: 2,
-            spaceBetween: 24,
+            spaceBetween: 0,
             loop: true,
             navigation: {
                 nextEl: ".swiper-button-next",
@@ -51,15 +51,36 @@ $(document).ready(function () {
                 el: ".swiper-pagination",
                 dynamicBullets: true,
             },
+            breakpoints: {
+                640: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+                },
+                991: {
+                  slidesPerView: 1,
+                  spaceBetween: 40,
+                },
+                1200: {
+                  slidesPerView: 2,
+                },
+              },
         });
     }
 
-
-
+    // readmore
+    $('.prreadmore.rm1').click(function(){
+        $(this).toggleClass('active');
+        $(this).find('span').toggle();
+        $(this).prev('.productinfo__boxdescr').toggleClass('show');
+    });
+    $('.prreadmore.rm2').click(function(){
+        $(this).toggleClass('active');
+        $(this).find('span').toggle();
+        $(this).prev('.row').toggleClass('show');
+    });
 
 
     // ------------------------------
-
 
 
 
